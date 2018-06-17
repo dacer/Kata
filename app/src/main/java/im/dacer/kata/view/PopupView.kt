a2904e209cb63511da0ae01e6c1813f0c66a34a1
Dacer <qqwerrd@gmail.com>
2018-06-17 16:21:55 +0800

    better copy button in intro view

--- app/src/main/java/im/dacer/kata/view/PopupView.kt
+++ app/src/main/java/im/dacer/kata/view/PopupView.kt
@@ -2,12 +2,13 @@ package im.dacer.kata.view
 
 import android.content.Context
 import android.graphics.*
-import android.graphics.drawable.NinePatchDrawable
+import android.support.v4.content.ContextCompat
 import android.util.AttributeSet
 import android.view.MotionEvent
 import android.view.View
 import im.dacer.kata.R
 import im.dacer.kata.util.ViewUtil
+import org.jetbrains.anko.dip
 
 
 /**
@@ -17,18 +18,23 @@ import im.dacer.kata.util.ViewUtil
 class PopupView @JvmOverloads constructor(
         context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0) : View(context, attrs, defStyleAttr) {
 
-    private val popupTextPaint = Paint()
+    private val popupTextPaint = Paint(Paint.ANTI_ALIAS_FLAG)
+    private val redPaint = Paint(Paint.ANTI_ALIAS_FLAG)
     private var point: Point? = null
     private val popupStr: String by lazy { context.getString(R.string.copy) }
-    private var popupRect: Rect? = null
+    private val popupHeight = dip(30)
+    private val popUpWidth = dip(50)
+    private val arrowHeight = dip(10)
+    private val arrowWidth = dip(12)
+
     var listener: PopupListener? = null
 
     init {
-        popupTextPaint.isAntiAlias = true
         popupTextPaint.color = Color.WHITE
-        popupTextPaint.textSize = ViewUtil.sp2px(16f).toFloat()
+        popupTextPaint.textSize = ViewUtil.sp2px(16f)
         popupTextPaint.strokeWidth = 5f
         popupTextPaint.textAlign = Paint.Align.CENTER
+        redPaint.color = ContextCompat.getColor(context, R.color.colorAccent)
     }
 
     override fun onDraw(canvas: Canvas) {
@@ -49,30 +55,14 @@ class PopupView @JvmOverloads constructor(
     }
 
     private fun drawPopup(canvas: Canvas, point: Point) {
-        val numStr = popupStr
-        val sidePadding = ViewUtil.dpToPx(8)
-        val x = point.x
-        val y = point.y
-        val popupTextRect = Rect()
-        popupTextPaint.getTextBounds(numStr, 0, numStr.length, popupTextRect)
-        popupRect = Rect(
-                x - popupTextRect.width() / 2 - sidePadding,
-                y - popupTextRect.height() - bottomTriangleHeight - popupTopPadding * 2 - popupBottomMargin,
-                x + popupTextRect.width() / 2 + sidePadding,
-                y + popupTopPadding + popupBottomPadding)
-
-
-        val popup = resources.getDrawable(R.drawable.popup_red) as NinePatchDrawable
-        popup.bounds = popupRect
-        popup.draw(canvas)
-        canvas.drawText(numStr, x.toFloat(), (y - bottomTriangleHeight - popupBottomMargin).toFloat(), popupTextPaint)
+        canvas.drawRoundRect(getBtnBounds(point), dip(6).toFloat(), dip(6).toFloat(), redPaint)
+        canvas.drawPath(getArrowPath(point), redPaint)
+        canvas.drawRectText(popupStr, getBtnBounds(point), popupTextPaint)
     }
-
-
 
     override fun onTouchEvent(event: MotionEvent?): Boolean {
         if (event?.action == MotionEvent.ACTION_UP) {
-            if (popupRect?.contains(event.x.toInt(), event.y.toInt()) == true) {
+            if (getBtnBounds(point).expandBounds(dip(20)).contains(event.x, event.y)) {
                 listener?.onPopupClicked()
             }
             hide()
@@ -80,15 +70,36 @@ class PopupView @JvmOverloads constructor(
         return popupIsShown
     }
 
+    private fun getArrowPath(point: Point): Path {
+        val result = Path()
+        result.moveTo(point.x.toFloat() - arrowWidth/2, point.y.toFloat())
+        result.lineTo(point.x.toFloat() - arrowWidth/2, getBtnBounds(point).bottom)
+        result.lineTo(point.x.toFloat() + arrowWidth/2, getBtnBounds(point).bottom)
+        result.lineTo(point.x.toFloat() - arrowWidth/2, point.y.toFloat())
+        return result
+    }
+
+    private fun getBtnBounds(point: Point?): RectF {
+        if (point == null) return RectF()
+        return RectF(point.x - popUpWidth / 2f,
+                (point.y - popupHeight).toFloat() - arrowHeight,
+                point.x + popUpWidth / 2f,
+                point.y.toFloat() - arrowHeight)
+    }
+
+    private fun RectF.expandBounds(px: Int): RectF {
+        return RectF(left - px, top - px, right + px, bottom + px)
+    }
+
+    private fun Canvas.drawRectText(text: String, r: RectF, paint: Paint) {
+        val numOfChars = paint.breakText(text, true, width.toFloat(), null)
+        val start = (text.length - numOfChars) / 2
+        drawText(text, start, start + numOfChars, r.centerX(), r.centerY() -
+                (paint.descent() + paint.ascent()) / 2, paint)
+    }
 
     interface PopupListener {
         fun onPopupClicked()
     }
 
-    companion object {
-        private val bottomTriangleHeight = 12
-        private val popupTopPadding = ViewUtil.dpToPx(2)
-        private val popupBottomPadding = ViewUtil.dpToPx(2)
-        private val popupBottomMargin = ViewUtil.dpToPx(5)
-    }
 }
