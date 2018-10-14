42096b0eaf6fb411f99447bd8d7e1ef950087216
Dacer <qqwerrd@gmail.com>
2018-10-15 01:10:54 +0900

    finished delete word function

--- app/src/main/java/im/dacer/kata/ui/main/wordbook/WordBookPresenter.kt
+++ app/src/main/java/im/dacer/kata/ui/main/wordbook/WordBookPresenter.kt
@@ -2,20 +2,22 @@ package im.dacer.kata.ui.main.wordbook
 
 import android.content.Context
 import android.graphics.Canvas
-import android.support.design.widget.Snackbar
 import android.support.v7.widget.RecyclerView
 import com.chad.library.adapter.base.listener.OnItemSwipeListener
 import im.dacer.kata.R
 import im.dacer.kata.data.local.MultiprocessPref
 import im.dacer.kata.data.local.SettingUtility
 import im.dacer.kata.data.model.bigbang.Word
+import im.dacer.kata.data.room.dao.ContextStrDao
 import im.dacer.kata.data.room.dao.WordDao
 import im.dacer.kata.injection.ConfigPersistent
 import im.dacer.kata.injection.qualifier.ApplicationContext
 import im.dacer.kata.ui.base.BasePresenter
+import im.dacer.kata.util.SnackBarHelper
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.Disposable
 import javax.inject.Inject
+
 
 @ConfigPersistent
 class WordBookPresenter @Inject constructor(@ApplicationContext val context: Context) : BasePresenter<WordBookMvp>() {
@@ -23,24 +25,26 @@ class WordBookPresenter @Inject constructor(@ApplicationContext val context: Con
     @Inject lateinit var settingUtility: SettingUtility
     @Inject lateinit var appPref: MultiprocessPref
     @Inject lateinit var wordDao: WordDao
+    @Inject lateinit var contextStrDao: ContextStrDao
 
     private var refreshWordDis: Disposable? = null
     private var wordList: List<Word>? = null
 
-    val swipeListener = object: OnItemSwipeListener {
+    val swipeListener = object : OnItemSwipeListener {
 
         override fun clearView(viewHolder: RecyclerView.ViewHolder?, pos: Int) {}
 
         override fun onItemSwiped(viewHolder: RecyclerView.ViewHolder?, pos: Int) {
             val word = wordList?.get(pos)
             if (word != null) {
-                wordDao.delete(word)
-                Snackbar.make(mvpView!!.getDecorView(), context.getString(R.string.deleted_sth, word.baseForm), Snackbar.LENGTH_SHORT)
-                        .setAction(R.string.redo) {
-                            wordDao.insert(word)
-                            refreshWordList()
-                        }
-                        .show()
+                SnackBarHelper.show(context, mvpView!!.getDecorView(), context.getString(R.string.deleted_sth, word.baseForm), { _ ->
+                    contextStrDao.findByWordId(word.id).subscribe { contextStrList ->
+                        contextStrList.forEach{ contextStrDao.delete(it) }
+                    }
+                    wordDao.delete(word)
+                }) {
+                    refreshWordList()
+                }
             }
         }
 
@@ -62,11 +66,11 @@ class WordBookPresenter @Inject constructor(@ApplicationContext val context: Con
 
     }
 
-    fun refreshWordList() {
+    private fun refreshWordList() {
         refreshWordDis?.dispose()
         refreshWordDis = wordDao.loadNotMasteredFlowable()
                 .observeOn(AndroidSchedulers.mainThread())
-                .subscribe{ wordList ->
+                .subscribe { wordList ->
                     this.wordList = wordList
                     mvpView?.showWords(wordList)
                 }
---
+++ app/src/main/java/im/dacer/kata/util/SnackBarHelper.kt
@@ -0,0 +1,38 @@
+package im.dacer.kata.util
+
+import android.content.Context
+import android.support.design.widget.Snackbar
+import android.view.View
+import android.widget.FrameLayout
+import com.mikepenz.materialize.util.UIUtils
+import im.dacer.kata.R
+
+class SnackBarHelper {
+    companion object {
+        fun show(context: Context, decorView: View, string: String,
+                 timeoutDismissListener: (Unit) -> (Unit), redoListener: (Unit) -> (Unit)) {
+            val snackBar = Snackbar.make(decorView, string, Snackbar.LENGTH_LONG)
+                    .setAction(R.string.redo) { redoListener(Unit) }
+            val navBarHeight = UIUtils.getNavigationBarHeight(context)
+            if (navBarHeight > 0) {
+                val snackBarView = snackBar.view as FrameLayout
+                val params = snackBarView.getChildAt(0).layoutParams as FrameLayout.LayoutParams
+                params.setMargins(params.leftMargin,
+                        params.topMargin,
+                        params.rightMargin,
+                        params.bottomMargin + navBarHeight)
+
+                snackBarView.getChildAt(0).layoutParams = params
+            }
+            snackBar.addCallback(object : Snackbar.Callback() {
+                override fun onDismissed(transientBottomBar: Snackbar?, event: Int) {
+                    super.onDismissed(transientBottomBar, event)
+                    if (event == DISMISS_EVENT_TIMEOUT) {
+                        timeoutDismissListener(Unit)
+                    }
+                }
+            })
+            snackBar.show()
+        }
+    }
+}
