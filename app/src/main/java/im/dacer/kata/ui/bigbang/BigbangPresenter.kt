449ecb0ccc09a777ec227178314e9d19d7c80401
Dacer <qqwerrd@gmail.com>
2018-11-11 23:36:01 +0900

    use google translation if no meaning in dict

--- app/src/main/java/im/dacer/kata/ui/bigbang/BigbangPresenter.kt
+++ app/src/main/java/im/dacer/kata/ui/bigbang/BigbangPresenter.kt
@@ -8,6 +8,7 @@ import im.dacer.kata.data.local.MultiprocessPref
 import im.dacer.kata.data.local.SearchDictHelper
 import im.dacer.kata.data.model.bigbang.History
 import im.dacer.kata.data.model.bigbang.Word
+import im.dacer.kata.data.model.segment.CombinedResult
 import im.dacer.kata.data.model.segment.KanjiResult
 import im.dacer.kata.data.room.dao.ContextStrDao
 import im.dacer.kata.data.room.dao.HistoryDao
@@ -17,9 +18,11 @@ import im.dacer.kata.injection.qualifier.ApplicationContext
 import im.dacer.kata.ui.base.BasePresenter
 import im.dacer.kata.util.ContextFinder
 import im.dacer.kata.util.LangUtils
+import im.dacer.kata.util.LangUtils.Companion.LANG_JAPANESE_KEY
 import im.dacer.kata.util.engine.SearchEngine
 import im.dacer.kata.util.helper.TTSHelper
 import im.dacer.kata.util.segment.BigBang
+import io.reactivex.Observable
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.Disposable
 import javax.inject.Inject
@@ -116,6 +119,8 @@ class BigbangPresenter @Inject constructor(@ApplicationContext val context: Cont
         onWordSelectedByUser(index)
         dictDisposable?.dispose()
         dictDisposable = searchDictHelper.searchForCombineResult(strForSearch, langUtils)
+                .flatMap { translateDirectlyIfNoMeaning(strForSearch, it) }
+                .observeOn(AndroidSchedulers.mainThread())
                 .subscribe({
                     val meaningStr = it.meaningStr
                     mvpView?.meaningText = if (meaningStr.isBlank()) {
@@ -128,6 +133,17 @@ class BigbangPresenter @Inject constructor(@ApplicationContext val context: Cont
                     }
                 }, { mvpView?.toastError(it) })
         mvpView?.showSystemUI()
+    }
+
+    private fun translateDirectlyIfNoMeaning(strForSearch: String, combinedResult: CombinedResult):
+            Observable<CombinedResult> {
+        if (combinedResult.meaningStr.isBlank()) {
+            return langUtils.translateOnline(fromLangLang = LANG_JAPANESE_KEY, sourceStr = strForSearch).map {
+                combinedResult.meaningStr = "$it\n(${context.getString(R.string.translated_by_google)})"
+                return@map combinedResult
+            }
+        }
+        return Observable.just(combinedResult)
     }
 
     fun onClickSearch() : Boolean {
--- app/src/main/java/im/dacer/kata/util/LangUtils.kt
+++ app/src/main/java/im/dacer/kata/util/LangUtils.kt
@@ -20,8 +20,7 @@ import javax.inject.Singleton
 class LangUtils @Inject constructor(private val appPre: MultiprocessPref) {
 
     fun fetchTranslation(dictEntry: DictEntry): Observable<String> {
-        var targetLang = appPre.targetLang
-        if (!LANG_KEY_LIST.contains(targetLang)) targetLang = LANG_ENGLISH_KEY
+        val targetLang = getTargetLang()
 
         //since people in China cannot use Google..
         if (targetLang == LANG_CHINESE_KEY) {
@@ -32,30 +31,41 @@ class LangUtils @Inject constructor(private val appPre: MultiprocessPref) {
             return Observable.just(dictEntry.gloss())
         }
 
-        return Rx2AndroidNetworking.get(getTranslationUrl(targetLang, dictEntry.gloss()!!))
+        return translateOnline(dictEntry.gloss()!!, targetLang)
+                .map { return@map it
+                        .replace("，", ", ")
+                        .split(", ")
+                        .distinct()
+                        .joinToString(", ") }
+    }
+
+    fun translateOnline(sourceStr: String, fromLangLang: String = LANG_ENGLISH_KEY, targetLang: String = getTargetLang()): Observable<String> {
+        return Rx2AndroidNetworking.get(getTranslationUrl(fromLangLang, targetLang, sourceStr))
                 .build()
                 .stringObservable
                 .map {
                     val array = JSONArray(it)
-                    val result = array.getJSONArray(0).getJSONArray(0).getString(0)
-                    return@map result
-                            .replace("，", ", ")
-                            .split(", ")
-                            .distinct()
-                            .joinToString(", ")
+                    return@map array.getJSONArray(0).getJSONArray(0).getString(0)
                 }
                 .subscribeOn(Schedulers.io())
+    }
+
+    private fun getTargetLang(): String {
+        var targetLang = appPre.targetLang
+        if (!LANG_KEY_LIST.contains(targetLang)) targetLang = LANG_ENGLISH_KEY
+        return targetLang
     }
 
     companion object {
         const val LANG_ENGLISH_KEY = "en"
         const val LANG_CHINESE_KEY = "zh-CN"
+        const val LANG_JAPANESE_KEY = "ja"
         private const val LANG_ENGLISH_VALUE = "English"
 
-        fun getTranslationUrl(target: String, source: String) =
-                "https://translate.googleapis.com/translate_a/single?client=gtx&sl=en&tl=$target&dt=t&q=${source.urlEncode()}"
+        private fun getTranslationUrl(fromLang: String, targetLang: String, sourceStr: String) =
+                "https://translate.googleapis.com/translate_a/single?client=gtx&sl=$fromLang&tl=$targetLang&dt=t&ie=UTF-8&oe=UTF-8&q=${sourceStr.urlEncode()}"
 
-        fun isZhCN(): Boolean {
+        private fun isZhCN(): Boolean {
             return Locale.getDefault().language == "zh" && Locale.getDefault().country == "CN"
 
         }
@@ -70,8 +80,6 @@ class LangUtils @Inject constructor(private val appPre: MultiprocessPref) {
         }
         val LANG_LIST = arrayOf(LANG_ENGLISH_VALUE, "Afrikaans", "Albanian", "Arabic", "Azerbaijani", "Basque", "Belarusian", "Bengali", "Bulgarian", "Catalan", "Chinese Simplified", "Chinese Traditional ", "Croatian", "Czech", "Danish", "Dutch", "Esperanto", "Estonian", "Filipino", "Finnish", "French", "Galician", "Georgian", "German", "Greek", "Gujarati", "Haitian Creole", "Hebrew", "Hindi", "Hungarian", "Icelandic", "Indonesian", "Irish", "Italian", "Japanese", "Kannada", "Korean", "Latin", "Latvian", "Lithuanian", "Macedonian", "Malay", "Maltese", "Norwegian", "Persian", "Polish", "Portuguese", "Romanian", "Russian", "Serbian", "Slovak", "Slovenian", "Spanish", "Swahili", "Swedish", "Tamil", "Telugu", "Thai", "Turkish", "Ukrainian", "Urdu", "Vietnamese", "Welsh", "Yiddish")
         val LANG_KEY_LIST = arrayOf(LANG_ENGLISH_KEY, "af", "sq", "ar", "az", "eu", "be", "bn", "bg", "ca", "zh-CN", "zh-TW", "hr", "cs", "da", "nl", "eo", "et", "tl", "fi", "fr", "gl", "ka", "de", "el", "gu", "ht", "iw", "hi", "hu", "is", "id", "ga", "it", "ja", "kn", "ko", "la", "lv", "lt", "mk", "ms", "mt", "no", "fa", "pl", "pt", "ro", "ru", "sr", "sk", "sl", "es", "sw", "sv", "ta", "te", "th", "tr", "uk", "ur", "vi", "cy", "yi")
-
-
 
         var DEFAULT_TARGET_LANG_KEY: String = LANG_ENGLISH_KEY
             get() = if (isZhCN()) LANG_CHINESE_KEY else LANG_ENGLISH_KEY
