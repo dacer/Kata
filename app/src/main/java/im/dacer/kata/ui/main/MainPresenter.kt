62c7ef32f22f4daf99e694b7762029ec35ee0653
Dacer <qqwerrd@gmail.com>
2019-09-28 19:59:08 +0900

    show lyric menu if easterEgg is actived

--- app/src/main/java/im/dacer/kata/ui/main/MainPresenter.kt
+++ app/src/main/java/im/dacer/kata/ui/main/MainPresenter.kt
@@ -5,6 +5,7 @@ import com.mikepenz.materialdrawer.model.DividerDrawerItem
 import com.mikepenz.materialdrawer.model.SecondaryDrawerItem
 import com.mikepenz.materialdrawer.model.interfaces.IDrawerItem
 import im.dacer.kata.R
+import im.dacer.kata.data.local.MultiprocessPref
 import im.dacer.kata.data.local.SettingUtility
 import im.dacer.kata.injection.ConfigPersistent
 import im.dacer.kata.injection.qualifier.ApplicationContext
@@ -19,6 +20,7 @@ import javax.inject.Inject
 @ConfigPersistent
 class MainPresenter @Inject constructor(@ApplicationContext val context: Context) : BasePresenter<MainMvp>() {
     @Inject lateinit var settingUtility: SettingUtility
+    @Inject lateinit var appPref: MultiprocessPref
 
     fun onResume() {
         restartListenService()
@@ -33,20 +35,23 @@ class MainPresenter @Inject constructor(@ApplicationContext val context: Context
     fun getDrawerItems(): Array<IDrawerItem<*, *>> {
         val itemInbox = SecondaryDrawerItem().withIdentifier(DrawerItem.INBOX.id).withName(R.string.inbox)
         val itemWordBook = SecondaryDrawerItem().withIdentifier(DrawerItem.WORD_BOOK.id).withName(R.string.word_book)
-//        val itemLyric = SecondaryDrawerItem().withIdentifier(DrawerItem.LYRIC.id).withName(R.string.lyric).withSelectable(false)
+        val itemLyric = SecondaryDrawerItem().withIdentifier(DrawerItem.LYRIC.id).withName(R.string.lyric).withSelectable(false)
         val itemNhkEasy = SecondaryDrawerItem().withIdentifier(DrawerItem.NHK_EASY.id).withName(R.string.nhk_news_easy)
         val itemNhk = SecondaryDrawerItem().withIdentifier(DrawerItem.NHK.id).withName(R.string.nhk_news)
         val itemSettings = SecondaryDrawerItem().withIdentifier(DrawerItem.SETTINGS.id).withName(R.string.settings).withSelectable(false)
         val itemAbout = SecondaryDrawerItem().withIdentifier(DrawerItem.ABOUT.id).withName(R.string.about).withSelectable(false)
-        return arrayOf(itemNhkEasy,
+        val result = arrayListOf<IDrawerItem<*, *>>(
+                itemNhkEasy,
                 itemNhk,
                 DividerDrawerItem(),
                 itemInbox,
                 itemWordBook,
-//                itemLyric,
                 itemSettings,
                 itemAbout,
                 DividerDrawerItem())
+
+        if (appPref.easterEgg) result.add(5, itemLyric)
+        return result.toTypedArray()
     }
 
     enum class DrawerItem(val id: Long) {
